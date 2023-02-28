db4afeb266dc93af63870ddbce7140a086eb8411
Nathan-Morrissey <nathan2004morrissey@gmail.com>
2023-02-28 20:56:58 +0000

    stopwatch

---
+++ script.js
@@ -0,0 +1,51 @@
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timeRef = document.querySelector(".time");
+let interval = null;
+
+document.getElementById("start").addEventListener("click", 
+() => {
+    if(interval !== null) {
+        clearInterval(interval);
+    }
+    interval = setInterval(displayTimer, 10);
+});
+
+document.getElementById("pause").addEventListener("click", 
+() => {
+    clearInterval(interval);
+});
+
+document.getElementById("reset").addEventListener("click", 
+() => {
+    clearInterval(interval);
+    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+    timeRef.innerHTML = "00 : 00: 00: 000";
+});
+
+function displayTimer () {
+    milliseconds += 10;
+    if(milliseconds == 1000) {
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60) {
+            seconds = 0;
+            if(minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms =
+    milliseconds < 10
+    ? "00" + milliseconds
+    : milliseconds < 100
+    ? "0" + milliseconds
+    : milliseconds;
+
+    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+
+}
