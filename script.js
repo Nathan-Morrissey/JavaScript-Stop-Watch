5f015ace555d782c8e01f04d712f47dca4f93b7d
Nathan-Morrissey <nathan2004morrissey@gmail.com>
2023-03-03 17:31:23 +0000

    fixed minutes coutner

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function displayTimer () {
         seconds++;
         if(seconds == 60) {
             seconds = 0;
+            minutes++;
             if(minutes == 60) {
                 minutes = 0;
                 hours++;
